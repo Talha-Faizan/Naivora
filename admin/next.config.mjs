15fda4bdf6e9f1466bd6fd4a2626f23a4b43851e
AsYnc <talhafaizan123456@gmail.com>
2026-08-23 17:23:23 +0530

    fix: allow ImageKit domains in admin next.config

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.29.49'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
